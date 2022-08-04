20838b66d893ea9bc9f427b240c52e1a92649696
joelmukanya <joel@lifechoices.co.za>
2022-08-04 20:40:44 +0200

    Updates

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ router.get('/', (req, res)=> {
 // Get users
 router.get('/users', (req, res)=> {
     let strQry =
-    `SELECT firstname, lastname, gender, address, userRole, email, userpassword
+    `SELECT id, firstname, lastname, gender, address, userRole, email, userpassword
     FROM users`;
     db.query(strQry, (err, results)=> {
         if(err) throw err; 
