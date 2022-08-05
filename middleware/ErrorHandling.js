cb34567c4fd62664182d49bafcb96efa154d1af3
joelmukanya <joel@lifechoices.co.za>
2022-08-05 15:28:09 +0200

    Let check

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 function errorHandling(err, req, res, next) {
-    if(err && (err.status >= 100 && err.status < 600 )){
+    res.locals.error = err;
+    if(err.status >= 100 && err.status < 600){
         res.status(err.status).json(
             {
                 msg: "An error occurred. Please try again later."
             }
         )
+    }else {
+        res.status(500);
     }
-    // Go to the next middleware
+    res.render('error');
     next();
 }
 module.exports = errorHandling;
