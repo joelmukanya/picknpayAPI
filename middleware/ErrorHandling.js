7707671e755dd4cd3d86d9a1566832fc523bf326
joelmukanya <joel@lifechoices.co.za>
2022-08-05 15:37:33 +0200

    Updates

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 function errorHandling(err, req, res, next) {
-    res.locals.error = err;
-    if(err.status >= 100 && err.status < 600){
-        res.status(err.status).json(
-            {
-                msg: "An error occurred. Please try again later."
-            }
-        )
-    }else {
-        res.status(500);
+    if(err){
+        if(err.status >= 100 && err.status < 600){
+            res.status(err.status).json(
+                {
+                    msg: "An error occurred. Please try again later."
+                }
+            )
+        }
     }
-    res.render('error');
     next();
 }
 module.exports = errorHandling;
