62816a5ebfa2ec194a7b08d2c59502a47e478f47
joelmukanya <joel@lifechoices.co.za>
2022-08-05 14:53:50 +0200

    Update

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.use((req, res, next)=>{
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", 
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    // response.setHeader("Access-Control-Allow-Headers", 
+    // "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 app.use(router, cors(), express.json(), 
