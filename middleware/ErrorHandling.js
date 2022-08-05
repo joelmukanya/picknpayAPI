90e3765c200f60eb2bf719e615626c1b8fab6ee3
joelmukanya <joel@lifechoices.co.za>
2022-08-05 15:21:04 +0200

    Update

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 function errorHandling(err, req, res, next) {
-    if(err){
+    if(err && (err.status >= 100 && err.status < 600 )){
         res.status(err.status).json(
             {
                 msg: "An error occurred. Please try again later."
