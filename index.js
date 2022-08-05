346df96db7634d4298890142c487f9c4b19c1433
joelmukanya <joel@lifechoices.co.za>
2022-08-05 15:58:36 +0200

    Updates

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const port = parseInt(process.env.PORT) || 4000;
 // Set header
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 app.use(router, cors(), express.json(), 
